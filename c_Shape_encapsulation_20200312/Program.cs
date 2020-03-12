7879713cd772332746846adc54a2ffb217a27417
htw01118891 <huangtianwei1988@163.com>
2020-03-12 10:43:26 +0800

    Initial commit

---
+++ c_Shape_encapsulation_20200312/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_Shape_encapsulation_20200312
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public void Acceptdetails()
+        {
+            Console.WriteLine("请输入长度：");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入宽度：");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("长度： {0}", length);
+            Console.WriteLine("宽度： {0}", width);
+            Console.WriteLine("面积： {0}", GetArea());
+        }
+    }
+
+    //class Program
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
