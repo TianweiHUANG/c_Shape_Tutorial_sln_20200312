2d8982d67baa57c59d6c44061eef3a5ecdb5a64b
htw01118891 <huangtianwei1988@163.com>
2020-03-21 15:57:39 +0800

    c_Shape_Tutorial_sln_20200312

---
+++ TCP_Socket_Client_TEST_20200321/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCP_Socket_Client_TEST_20200321
+{
+    /// <summary>
+    /// 客户端
+    /// </summary>
+    class Socket_Client
+    {
+        public Socket_Client(string ip, int port)
+        {
+            this.iPAddress = IPAddress.Parse(ip);
+            this.port = port;
+        }
+
+        /// <summary>
+        /// 套接字
+        /// </summary>
+        private Socket client_socket;
+        /// <summary>
+        /// 客户端要连接的ip地址
+        /// </summary>
+        private IPAddress iPAddress;
+        /// <summary>
+        /// 客户端要连接的端口好
+        /// </summary>
+        private int port;
+        /// <summary>
+        /// 创建客户端连接的套接字
+        /// </summary>
+        /// <returns></returns>
+        public Socket Create_Client_Socket()
+        {
+            return new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        /// <summary>
+        /// 连接服务器
+        /// </summary>
+        public void Connect_Server()
+        {
+            client_socket = Create_Client_Socket();
+            //tcp连接服务器的时候只需要连接一次，因为tcp是长链接
+            client_socket.Connect(new IPEndPoint(iPAddress, port));
+        }
+
+        /// <summary>
+        /// 接收来自服务器的消息
+        /// </summary>
+        public void Recv_Msg_By_Client()
+        {
+            while (true)
+            {
+                byte[] ser_msg = new byte[1024];
+                int count = client_socket.Receive(ser_msg);
+                string str_msg = Encoding.UTF8.GetString(ser_msg, 0, count);
+                if (count > 0)
+                {
+                    Console.WriteLine("接收到来自{0}的消息为：{1}", client_socket.RemoteEndPoint, str_msg);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 向服务器发送请求
+        /// </summary>
+        public void Request_Client()
+        {
+            while (true)
+            {
+                Console.WriteLine("请输入你要发送到服务器的消息：");
+                string send_msg = Console.ReadLine();
+                byte[] by_msg = Encoding.UTF8.GetBytes(send_msg);
+                client_socket.Send(by_msg);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入你要连接的服务器的ip地址：");
+            string ip = Console.ReadLine();
+            Console.WriteLine("请输入你要连接的服务器的端口号：");
+            int port = int.Parse(Console.ReadLine());
+
+            //创建套接字
+            Socket_Client s = new Socket_Client(ip, port);
+            //连接服务器
+            s.Connect_Server();
+            //接收服务器的消息
+            Thread recv = new Thread(s.Recv_Msg_By_Client);
+            //给服务器发送消息
+            Thread send = new Thread(s.Request_Client);
+            recv.Start();
+            send.Start();
+
+            recv.Join();
+            send.Join();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCP_Socket_Server_TEST_20200321/Program.cs
@@ -0,0 +1,167 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCP_Socket_Server_TEST_20200321
+{
+    /// <summary>
+    /// 服务端
+    /// </summary>
+    class Serversocket
+    {
+        /// <summary>
+        /// 服务端入口
+        /// </summary>
+        private static Serversocket socketserver;
+        public static Serversocket Instance()
+        {
+            if (socketserver == null)
+            {
+                socketserver = new Serversocket();
+            }
+            return socketserver;
+        }
+
+        private Socket server;
+        private IPAddress iPAddress;
+        private int port;
+
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        public void Init()
+        {
+            iPAddress = IPAddress.Parse("127.0.0.1");
+            port = 8888;
+            CreateSocket();
+            BindAndListen();
+            WaitClientConnection();
+        }
+
+        /// <summary>
+        /// 1.创建服务端的套接字
+        /// </summary>
+        private void CreateSocket()
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        }
+        /// <summary>
+        /// 2.绑定服务器ip和端口
+        /// </summary>
+        private void BindAndListen()
+        {
+            server.Bind(new IPEndPoint(iPAddress, port));
+            server.Listen(100);
+        }
+
+        /// <summary>
+        /// 用一个列表储存连接成功的客户端
+        /// </summary>
+        private List<Socket> ConnectSockeList = new List<Socket>();
+        /// <summary>
+        /// 用一个字典储存所有接受客户端消息以及发送给客户端消息的线程
+        /// </summary>
+        private Dictionary<int, List<Thread>> recvThreadList = new Dictionary<int, List<Thread>>();
+
+        /// <summary>
+        /// 3.等待客户端的连接
+        /// </summary>
+        private void WaitClientConnection()
+        {
+            int index = 1;
+            while (true)
+            {
+                Console.WriteLine("当前链接数量：" + recvThreadList.Count);
+                Console.WriteLine("等待客户端的链接：");
+                Socket ClientSocket = server.Accept();
+                if (ClientSocket != null)
+                {
+                    Console.WriteLine("{0}连接成功！", ClientSocket.RemoteEndPoint);
+                    ConnectSockeList.Add(ClientSocket);
+                    //创建接受客户端消息的线程，并将其启动
+                    Thread recv = new Thread(RecvMessage);
+                    recv.Start(new ArrayList { index, ClientSocket });
+                    Thread send = new Thread(SendMessage);
+                    send.Start(new ArrayList { index, ClientSocket });
+                    recvThreadList.Add(index, new List<Thread> { recv, send });
+                    index++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 接受客户端的消息
+        /// </summary>
+        /// <param name="clientsocket"></param>
+        private void RecvMessage(object client_socket)
+        {
+            ArrayList arraylist = client_socket as ArrayList;
+            int index = (int)arraylist[0];
+            Socket clientsocket = arraylist[1] as Socket;
+            while (true)
+            {
+                try
+                {
+                    byte[] strbyte = new byte[1024];
+                    int count = clientsocket.Receive(strbyte);
+                    string ret = Encoding.UTF8.GetString(strbyte, 0, count);
+                    Console.WriteLine("{0}给你发送了消息：{1}", clientsocket.RemoteEndPoint, ret);
+                }
+                catch (Exception)
+                {
+                    //客户端离去时终止线程
+                    Console.WriteLine("代号为:{0}的客户端已经离去！", index);
+                    recvThreadList[index][0].Abort();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 向客户端返回消息
+        /// </summary>
+        private void SendMessage(object client_socket)
+        {
+            ArrayList arraylist = client_socket as ArrayList;
+            int index = (int)arraylist[0];
+            Socket clientsocket = arraylist[1] as Socket;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("请输入要发送的消息：");
+                    string str = Console.ReadLine();
+                    byte[] strbyte = Encoding.UTF8.GetBytes(str);
+                    clientsocket.Send(strbyte);
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("代号为：{0}的客户端已经离去！消息发送失败！");
+                    recvThreadList[index][1].Abort();
+                    recvThreadList.Remove(index);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serversocket.Instance().Init();
+            Console.ReadKey();
+        }
+    }
+}
