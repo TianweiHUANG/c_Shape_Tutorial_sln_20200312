d46357e6af65a0e6ca8bc6d58d5ed0c62cfa7b1c
htw01118891 <huangtianwei1988@163.com>
2020-03-27 10:38:47 +0800

    c_Shape_Tutorial_sln_20200312

---
+++ DataTypeConversion_20200327/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTypeConversion_20200327
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- 隐式类型转换 -
+            int i = 75;
+            float f = 53.005f;
+            double d = 2345.7652;
+            bool b = true;
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(f.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(b.ToString());
+            //- 显式类型转换 - 
+            double d1 = 5673.74;
+            int i1;
+            i1 = (int)d1;
+            Console.WriteLine(i1);
+            Console.ReadKey();
+        }
+    }
+}
