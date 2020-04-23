23af5f497d484c97c175200ac7d27b8695b7c5b3
htw01118891 <huangtianwei1988@163.com>
2020-04-23 22:30:23 +0800

    c_Shape_Tutorial_sln_20200312

--- CSapp_XMP-6004_TEST_20200425/Program.cs
+++ CSapp_XMP-6004_TEST_20200425/Program.cs
@@ -11,9 +11,9 @@ namespace CSapp_XMP_6004_TEST_20200425
     {
         static void Main(string[] args)
         {
-            mySerialPort myPort = new mySerialPort();
-            myPort.Send();
-            myPort.Close();
+            //mySerialPort myPort = new mySerialPort();
+            //myPort.Send();
+            //myPort.Close();
 
             //************短路运算符************
             //Console.WriteLine("-&-运行结果为:");
@@ -28,6 +28,38 @@ namespace CSapp_XMP_6004_TEST_20200425
             //Console.WriteLine("-||-运行结果为:");
             //Console.WriteLine("firstMethod() || secondMethod():={0}", firstMethod() || secondMethod());
             //Console.ReadKey();
+
+            string str1 = "a";
+            byte[] array1 = System.Text.Encoding.ASCII.GetBytes(str1);
+            Console.WriteLine("HEX->{0 :X2}", array1[0]);
+            int asciicode1 = (int)(array1[0]);
+            string ASCIIstr1 = Convert.ToString(asciicode1);
+            Console.WriteLine("DEC->{0}", ASCIIstr1);
+            Console.ReadKey();
+
+            string str2 = "SessionN";
+            byte[] array2 = System.Text.Encoding.ASCII.GetBytes(str2);     
+            string ASCIIstr2 = null;
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine("HEX->{0 :X2}", array2[i]);
+                int asciicode2 = (int)(array2[i]);
+                ASCIIstr2 += Convert.ToString(asciicode2) + " ";
+            }
+            Console.WriteLine("DEC->{0}",ASCIIstr2);
+            Console.ReadKey();
+
+            //byte[] array3 = { 65 };
+            byte[] array3 = { 0X41 };
+            string str3 =
+                System.Text.Encoding.ASCII.GetString(array3);
+            Console.WriteLine(str3);
+            Console.ReadKey();
+
+            byte[] array4 = { 83, 101, 115, 115, 105, 111, 110, 78 };
+            string str4 = System.Text.Encoding.ASCII.GetString(array4);
+            Console.WriteLine(str4);
+            Console.ReadKey();
         }
         public static bool firstMethod()
         {
