3e1ed4258047ce033f04e565b03e6fb214a8a2c8
htw01118891 <huangtianwei1988@163.com>
2020-05-17 16:43:49 +0800

    ArduinoProjectHub_20200518

--- ArduinoProjectHub_20200518/FreeRTOS_TEST_20200518/FreeRTOS_TEST_20200518.ino
+++ ArduinoProjectHub_20200518/FreeRTOS_TEST_20200518/FreeRTOS_TEST_20200518.ino
@@ -29,12 +29,12 @@ void setup()
   //display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   //display.setTextColor(WHITE);//打开像素点发光
   //display.setTextSize(2);//设置字体大小  
-  //display.setCursor(15, 30);//设置显示位置 
+  //display.setCursor(15, 30);//设置显示位置
         
-  xTaskCreate(Task1_func, "Task1", 128, NULL, 1, NULL); //创建任务1
-  xTaskCreate(Task2_func, "Task2", 128, NULL, 2, NULL); //创建任务2
-  //xTaskCreate(Task3_func, "Task3", 128, NULL, 3, NULL); //创建任务3
-  //xTaskCreate(Task4_func, "Task4", 128, NULL, 4, NULL); //创建任务4
+  xTaskCreate(Task1_func, "Task1", 100, NULL, 1, NULL); //创建任务1
+  xTaskCreate(Task2_func, "Task2", 100, NULL, 1, NULL); //创建任务2
+  //xTaskCreate(Task3_func, "Task3", 100, NULL, 1, NULL); //创建任务3
+  //xTaskCreate(Task4_func, "Task4", 100, NULL, 1, NULL); //创建任务4
   vTaskStartScheduler(); //启动任务调度
 }
 
@@ -98,7 +98,7 @@ void Task2_func(void *param)
 //  while (1)
 //  { 
 //    if (OLEDdisplay!=OLEDdisplay_TEMP)
-//    {      
+//    { 
 //      display.clearDisplay();//清除显示  
 //      display.println(OLEDdisplay);         
 //      display.display();//打开显示
@@ -110,7 +110,7 @@ void Task2_func(void *param)
 //void Task4_func(void *param)
 //{
 //  while (1)
-//  { 
+//  {    
 //    display.startscrollright(0x00, 0x0F);
 //    vTaskDelay(5000 / portTICK_PERIOD_MS );//等待5秒
 //    display.stopscroll();
