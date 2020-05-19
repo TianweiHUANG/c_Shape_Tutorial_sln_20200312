ab7b0b0171c66e558752631146bf145ba6a90fad
htw01118891 <huangtianwei1988@163.com>
2020-05-19 21:05:38 +0800

    c_Shape_Tutorial_sln_20200312

--- ArduinoProjectHub_20200518/FSM_TEST_20200518/FSM_TEST_20200518.ino
+++ ArduinoProjectHub_20200518/FSM_TEST_20200518/FSM_TEST_20200518.ino
@@ -1,73 +1,32 @@
-//const int buttonPin07 = 7;
-//const int ledPin12 = 12;
-//volatile int stateFlag = 1;
-//
-//void setup()
-//{
-//  pinMode(buttonPin07,INPUT_PULLUP);
-//  pinMode(ledPin12, OUTPUT);
-//}
-//
-//void loop()
-//{
-//  switch (stateFlag)
-//  {
-//    case 1:
-//    digitalWrite(ledPin12, HIGH);
-//    if(!digitalRead(buttonPin07))stateFlag=2;
-//    break;
-//    
-//    case 2:
-//    digitalWrite(ledPin12, LOW);
-//    if(!digitalRead(buttonPin07))stateFlag=3;
-//    break;
-//    
-//    case 3:
-//    LED_Blink(1000);
-//    if(!digitalRead(buttonPin07))stateFlag=1;
-//    break;
-//    
-//    default:
-//    break;
-//  }
-//}
-//
-//void LED_Blink (unsigned int time_ms)
-//{
-//  static unsigned long int n=0;
-//  static int stateLed = LOW;
-//  if (millis()-n>=time_ms)
-//  {
-//    stateLed=!stateLed;
-//    digitalWrite(ledPin12, stateLed);
-//    n=millis();
-//  }
-//}
-
 const int buttonPin07 = 7;
 const int ledPin12 = 12;
+const int ledPin13 = 13;
 volatile int stateFlag_Task1 = 0;
-volatile unsigned long millis_Start1=0;
-volatile unsigned long millis_Start2=0;
+volatile int stateFlag_Task2 = 0;
+volatile unsigned long millis_Start1_1=0;
+volatile unsigned long millis_Start1_2=0;
+volatile unsigned long millis_Start2_1=0;
 
 void setup()
 {
   pinMode(buttonPin07,INPUT_PULLUP);
   pinMode(ledPin12, OUTPUT);
+  pinMode(ledPin13, OUTPUT);
 }
 
 void loop()
 {
+  //*** *** *** *** *** *** stateFlag_Task1 *** *** *** *** *** ***// 
   switch (stateFlag_Task1)
   {
     case 0:
       digitalWrite(ledPin12, HIGH);
-      millis_Start1=millis();
+      millis_Start1_1=millis();
       stateFlag_Task1=10;
       break;
 
     case 10:
-      if (millis()-millis_Start1>=1000)
+      if (millis()-millis_Start1_1>=1000)
       {
         stateFlag_Task1=20;
       }
@@ -75,17 +34,58 @@ void loop()
     
     case 20:
       digitalWrite(ledPin12, LOW);
-      millis_Start2=millis();
+      millis_Start1_2=millis();
       stateFlag_Task1=30;
       break;
 
     case 30:
-      if (millis()-millis_Start2>=1000)
+      if (millis()-millis_Start1_2>=1000)
       {
         stateFlag_Task1=0;
       }
       break;
       
+    default:
+      break;
+  }
+  
+  //*** *** *** *** *** *** stateFlag_Task2 *** *** *** *** *** ***// 
+  switch (stateFlag_Task2)
+  {
+    case 0:
+      if (digitalRead(buttonPin07) == LOW) 
+      {
+        millis_Start2_1=millis();
+        stateFlag_Task2=10;
+      }
+      break;
+
+    case 10:
+      if (digitalRead(buttonPin07) == LOW)
+      {
+        if (millis()-millis_Start2_1>=50)
+        {
+          stateFlag_Task2=20;
+        }
+      }
+      else
+      {  
+        stateFlag_Task2=0; 
+      }
+      break;
+    
+    case 20:
+      if (digitalRead(buttonPin07) == LOW)
+      {
+        digitalWrite(ledPin13, HIGH);
+      }
+      else
+      {  
+        digitalWrite(ledPin13, LOW); 
+        stateFlag_Task2=0; 
+      }
+      break;
+
     default:
       break;
   }
