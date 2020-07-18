0e88620631526cc84774a14ecc958bcee508b998
htw01118891 <huangtianwei1988@163.com>
2020-07-18 22:04:50 +0800

    c_Shape_Tutorial_sln_20200312

---
+++ TCPsocket通讯/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPsocket通讯
+{
+    public partial class Form1 : Form
+    {
+        delegate void AddOnlineDelegate(string  str ,bool bl);
+        delegate void RecMegDelegate(string str);
+        Dictionary<string, Socket> DicSocket = new Dictionary<string, Socket>();
+        public Form1()
+        {
+            InitializeComponent();
+            myAddOnlineDelegate = AddOnline;
+            myRecMegDelegate = AddRecMsg;
+        }
+        Socket socket = null;
+        Thread threadListen = null;
+        AddOnlineDelegate myAddOnlineDelegate;
+        RecMegDelegate myRecMegDelegate;
+        private void btn_StartServer_Click(object sender, EventArgs e)
+        {
+            socket = new Socket( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress iPAddress = IPAddress.Parse(txt_IP.Text.Trim());
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress,int.Parse(txt_Port.Text.Trim()));
+            try
+            {
+                socket.Bind(iPEndPoint);
+                MessageBox.Show("服务开启成功" );
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("服务开启失败"+ex);
+                return;
+            }
+            socket.Listen(10);
+            threadListen = new Thread(ListenConnecting1);
+            threadListen.IsBackground=true;
+            threadListen.Start();
+            btn_StartServer.Enabled = false;
+        }
+
+        private void ListenConnecting1()
+        {
+            while (true)
+            {
+                Socket socketClient = socket.Accept();
+                string client = socketClient.RemoteEndPoint.ToString();
+                DicSocket.Add(client,socketClient);
+               
+                Invoke(myAddOnlineDelegate, client,true);
+                
+                Thread thr = new Thread(RecieveMsg);
+                
+                thr.IsBackground = true;
+                thr.Start(socketClient);
+            }
+
+        }
+
+        private void RecieveMsg(object socketClient)
+        {
+            Socket scketClient = socketClient as Socket;
+            while (true)
+            {
+                byte[] arrMsgRec = new byte[1024 * 1024 * 2];
+                int length = -1;
+                try
+                {
+                    length = scketClient.Receive(arrMsgRec);
+
+                }
+                catch (Exception)
+                {
+
+                    string str = scketClient.RemoteEndPoint.ToString();
+                    Invoke(myRecMegDelegate, str + "下线了");
+                    Invoke(myAddOnlineDelegate,str,false);
+                    DicSocket.Remove(str);
+                    break;
+                }
+               
+                if (length == 0)
+                {
+                    string str = scketClient.RemoteEndPoint.ToString();
+                    Invoke(myAddOnlineDelegate, str, false) ;
+                    DicSocket.Remove(str);
+                    break;
+                }
+                else
+                {
+                    string strMsg = Encoding.UTF8.GetString(arrMsgRec,0, length);
+                    Invoke(myRecMegDelegate , strMsg+ Environment.NewLine);
+                }
+            }
+        }
+        #region 委托更新UI
+        private void AddOnline(string str,bool bl)
+        {
+            if (bl)
+            {
+                txt_Online.Items.Add(str);
+            }
+            else
+            {
+                txt_Online.Items.Remove(str);
+            }
+            
+        }
+        private void AddRecMsg(string str)
+        {
+            txt_Rcv.AppendText(str);
+        }
+        #endregion
+
+        private void btn_SendToSingle_Click(object sender, EventArgs e)
+        {
+            string StrMsg = this.txt_Send.Text.Trim();
+            byte[] arrMsg = Encoding.UTF8.GetBytes(StrMsg);
+            if (this.txt_Online.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("请选择要发送的对象");
+                return;
+            }
+            else
+            {
+                foreach (String item in this.txt_Online.SelectedItems)
+                {
+                    DicSocket[item].Send(arrMsg);
+                    string Msg = "[发送]  " + item + "   " + StrMsg;
+                    Invoke(myRecMegDelegate,Msg+Environment.NewLine);
+                }
+            }
+        }
+
+        private void OpenClient_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ WFapp_TcpSocket_20200718/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WFapp_TcpSocket_20200718
+{
+    partial class Form_TcpServer
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text_Rcv = new System.Windows.Forms.TextBox();
+            this.text_Send = new System.Windows.Forms.TextBox();
+            this.text_IP = new System.Windows.Forms.TextBox();
+            this.text_Port = new System.Windows.Forms.TextBox();
+            this.text_Status = new System.Windows.Forms.TextBox();
+            this.btn_Listen = new System.Windows.Forms.Button();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.btn_TcpClientForm = new System.Windows.Forms.Button();
+            this.lab_IP = new System.Windows.Forms.Label();
+            this.lab_Port = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // text_Rcv
+            // 
+            this.text_Rcv.Location = new System.Drawing.Point(16, 16);
+            this.text_Rcv.Multiline = true;
+            this.text_Rcv.Name = "text_Rcv";
+            this.text_Rcv.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.text_Rcv.Size = new System.Drawing.Size(216, 184);
+            this.text_Rcv.TabIndex = 0;
+            this.text_Rcv.TextChanged += new System.EventHandler(this.text_Rcv_TextChanged);
+            // 
+            // text_Send
+            // 
+            this.text_Send.Location = new System.Drawing.Point(16, 216);
+            this.text_Send.Multiline = true;
+            this.text_Send.Name = "text_Send";
+            this.text_Send.Size = new System.Drawing.Size(216, 208);
+            this.text_Send.TabIndex = 1;
+            this.text_Send.TextChanged += new System.EventHandler(this.text_Send_TextChanged);
+            // 
+            // text_IP
+            // 
+            this.text_IP.Location = new System.Drawing.Point(312, 40);
+            this.text_IP.Name = "text_IP";
+            this.text_IP.Size = new System.Drawing.Size(160, 21);
+            this.text_IP.TabIndex = 2;
+            this.text_IP.TextChanged += new System.EventHandler(this.text_IP_TextChanged);
+            // 
+            // text_Port
+            // 
+            this.text_Port.Location = new System.Drawing.Point(312, 88);
+            this.text_Port.Name = "text_Port";
+            this.text_Port.Size = new System.Drawing.Size(160, 21);
+            this.text_Port.TabIndex = 3;
+            this.text_Port.TextChanged += new System.EventHandler(this.text_Port_TextChanged);
+            // 
+            // text_Status
+            // 
+            this.text_Status.Location = new System.Drawing.Point(248, 136);
+            this.text_Status.Multiline = true;
+            this.text_Status.Name = "text_Status";
+            this.text_Status.Size = new System.Drawing.Size(224, 64);
+            this.text_Status.TabIndex = 4;
+            this.text_Status.TextChanged += new System.EventHandler(this.text_Status_TextChanged);
+            // 
+            // btn_Listen
+            // 
+            this.btn_Listen.Location = new System.Drawing.Point(248, 240);
+            this.btn_Listen.Name = "btn_Listen";
+            this.btn_Listen.Size = new System.Drawing.Size(224, 39);
+            this.btn_Listen.TabIndex = 5;
+            this.btn_Listen.Text = "Listen";
+            this.btn_Listen.UseVisualStyleBackColor = true;
+            this.btn_Listen.Click += new System.EventHandler(this.btn_Listen_Click);
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(248, 304);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(224, 40);
+            this.btn_Send.TabIndex = 6;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // btn_TcpClientForm
+            // 
+            this.btn_TcpClientForm.Location = new System.Drawing.Point(248, 368);
+            this.btn_TcpClientForm.Name = "btn_TcpClientForm";
+            this.btn_TcpClientForm.Size = new System.Drawing.Size(224, 40);
+            this.btn_TcpClientForm.TabIndex = 7;
+            this.btn_TcpClientForm.Text = "TcpClientForm";
+            this.btn_TcpClientForm.UseVisualStyleBackColor = true;
+            this.btn_TcpClientForm.Click += new System.EventHandler(this.btn_TcpClientForm_Click);
+            // 
+            // lab_IP
+            // 
+            this.lab_IP.AutoSize = true;
+            this.lab_IP.Location = new System.Drawing.Point(248, 40);
+            this.lab_IP.Name = "lab_IP";
+            this.lab_IP.Size = new System.Drawing.Size(35, 12);
+            this.lab_IP.TabIndex = 8;
+            this.lab_IP.Text = "TcpIP";
+            this.lab_IP.Click += new System.EventHandler(this.lab_IP_Click);
+            // 
+            // lab_Port
+            // 
+            this.lab_Port.AutoSize = true;
+            this.lab_Port.Location = new System.Drawing.Point(248, 88);
+            this.lab_Port.Name = "lab_Port";
+            this.lab_Port.Size = new System.Drawing.Size(47, 12);
+            this.lab_Port.TabIndex = 9;
+            this.lab_Port.Text = "TcpPort";
+            this.lab_Port.Click += new System.EventHandler(this.lab_Port_Click);
+            // 
+            // Form_TcpServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 442);
+            this.Controls.Add(this.lab_Port);
+            this.Controls.Add(this.lab_IP);
+            this.Controls.Add(this.btn_TcpClientForm);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.btn_Listen);
+            this.Controls.Add(this.text_Status);
+            this.Controls.Add(this.text_Port);
+            this.Controls.Add(this.text_IP);
+            this.Controls.Add(this.text_Send);
+            this.Controls.Add(this.text_Rcv);
+            this.Name = "Form_TcpServer";
+            this.Text = "Form_TcpServer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text_Rcv;
+        private System.Windows.Forms.TextBox text_Send;
+        private System.Windows.Forms.TextBox text_IP;
+        private System.Windows.Forms.TextBox text_Port;
+        private System.Windows.Forms.TextBox text_Status;
+        private System.Windows.Forms.Button btn_Listen;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.Button btn_TcpClientForm;
+        private System.Windows.Forms.Label lab_IP;
+        private System.Windows.Forms.Label lab_Port;
+    }
+}
+
