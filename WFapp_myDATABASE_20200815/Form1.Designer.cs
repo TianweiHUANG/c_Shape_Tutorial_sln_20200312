76e4f5a01a9527af8438a506b4d0c98c463e19f6
htw01118891 <huangtianwei1988@163.com>
2020-08-13 17:48:54 +0800

    WFapp_myDATABASE_20200815

---
+++ WFapp_myDATABASE_20200815/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WFapp_myDATABASE_20200815
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Select = new System.Windows.Forms.Button();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Select
+            // 
+            this.btn_Select.Location = new System.Drawing.Point(16, 256);
+            this.btn_Select.Name = "btn_Select";
+            this.btn_Select.Size = new System.Drawing.Size(72, 39);
+            this.btn_Select.TabIndex = 2;
+            this.btn_Select.Text = "Select";
+            this.btn_Select.UseVisualStyleBackColor = true;
+            this.btn_Select.Click += new System.EventHandler(this.btn_Select_Click);
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Location = new System.Drawing.Point(104, 256);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(72, 40);
+            this.btn_Insert.TabIndex = 3;
+            this.btn_Insert.Text = "Insert";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(192, 256);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(72, 40);
+            this.btn_Delete.TabIndex = 4;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Location = new System.Drawing.Point(280, 256);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(72, 40);
+            this.btn_Update.TabIndex = 5;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 312);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Insert);
+            this.Controls.Add(this.btn_Select);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Select;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Update;
+    }
+}
+
---
+++ WFapp_myDATABASE_20200815/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Data.SQLite;
+
+namespace WFapp_myDATABASE_20200815
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Select_Click(object sender, EventArgs e)
+        {
+            //using (var myDataBase_connection = new SQLiteConnection(@"data source=C:\Users\Administrator\Desktop\myDataBase.db"))
+            using (var myDataBase_connection = new SQLiteConnection("data source=myDataBase.db"))
+            {
+                myDataBase_connection.Open();
+                var command = new SQLiteCommand("select * from myTable", myDataBase_connection);
+                var adapter = new SQLiteDataAdapter(command);
+                var dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                var table = dataSet.Tables[0];
+            }
+        }
+
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            //using (var myDataBase_connection = new SQLiteConnection(@"data source=C:\Users\Administrator\Desktop\myDataBase.db"))
+            using (var myDataBase_connection = new SQLiteConnection("data source=myDataBase.db"))
+            {
+                myDataBase_connection.Open();
+                var command = new SQLiteCommand("insert into myTable " + "values(3,\"张三\",22,\"陕西省西安市长安区\",\"看书,听音乐\")", myDataBase_connection);
+                var result = command.ExecuteNonQuery();
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            //using (var myDataBase_connection = new SQLiteConnection(@"data source=C:\Users\Administrator\Desktop\myDataBase.db"))
+            using (var myDataBase_connection = new SQLiteConnection("data source=myDataBase.db"))
+            {
+                myDataBase_connection.Open();
+                var command = new SQLiteCommand("delete from myTable where Id = 3", myDataBase_connection);
+                var result = command.ExecuteNonQuery();
+            }
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            //using (var myDataBase_connection = new SQLiteConnection(@"data source=C:\Users\Administrator\Desktop\myDataBase.db"))
+            using (var myDataBase_connection = new SQLiteConnection("data source=myDataBase.db"))
+            {
+                myDataBase_connection.Open();
+                var command = new SQLiteCommand("update myTable set Name = '王五' where Id = 3",
+                    myDataBase_connection);
+                var result = command.ExecuteNonQuery();
+            }
+        }
+    }
+}
